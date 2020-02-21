b53ec413c309cdebfd5a9822604c5dfb4830488f
vmipfdr <paynemw@yahoo.com>
2020-02-21 16:11:57 -0500

    got storybook-static

---
+++ storybook-static/precache-manifest.ac360e72aaeea594fe62f70f213ddc8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec68ec574de36f97dbe16a7c00b7edc",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1301b4c9e0e0f97bf9b2.bundle.js"
+  },
+  {
+    "url": "runtime~main.1301b4c9e0e0f97bf9b2.bundle.js"
+  },
+  {
+    "revision": "b81d5c4c806bd1526db0",
+    "url": "static/css/main.178d5943.chunk.css"
+  },
+  {
+    "url": "vendors~main.1301b4c9e0e0f97bf9b2.bundle.js"
+  },
+  {
+    "url": "vendors~main.1301b4c9e0e0f97bf9b2.bundle.js.LICENSE.txt"
+  }
+]);
