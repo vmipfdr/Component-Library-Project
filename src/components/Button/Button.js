571e4738fede3d20b7a3d87d2572e2d9f1fce01d
vmipfdr <paynemw@yahoo.com>
2020-02-21 16:07:02 -0500

    half way through buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
+import "./Button.css";
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = props => {
+  let classList = "";
+  let types = [
+    "primary",
+    "danger",
+    "success",
+    "warning",
+    "default",
+    "inverse-primary",
+    "inverse-danger",
+    "inverse-success",
+    "inverse-warning",
+    "inverse-default"
+  ];
+  if (types.includes(props.type)) {
+    // these class lists must take back ticks
+    classList += ` button-${props.type}`;
+  }
+  if (props.large) {
+    classList += ` button-large`;
+  }
+  return <button className={classList}>{props.label}</button>;
+};
+// see above, the style props.style comes from the button.stories.js and outerHTML also comes from the button stories
 
-export default Button; 
+export default Button;
+
+//this will add an eventlistener to the button you deploy
+//<button className={classList} onClick={props.onClick}>{props.label}</button>
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,38 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "./Button";
 
-import Button from './Button';
-
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+storiesOf("Button", module)
+  .add("Primary", () => <Button label="Primary" type="primary" />)
+  .add("Danger", () => <Button label="Danger" type="danger" />)
+  .add("Success", () => <Button label="Success" type="success" />)
+  .add("Warning", () => <Button label="Warning" type="warning" />)
+  .add("Default", () => <Button label="Default" type="default" />)
+  .add("Large Primary", () => (
+    <Button label="Large Primary" type="primary" large />
+  ))
+  .add("Large Danger", () => (
+    <Button label="Large Danger" type="danger" large />
+  ))
+  .add("Large Success", () => (
+    <Button label="Large Success" type="success" large />
+  ))
+  .add("Large Warning", () => (
+    <Button label="Large Warning" type="warning" large />
+  ))
+  .add("Large Default", () => (
+    <Button label="Large Default" type="default" large />
+  ))
+  .add("Inverse Primary", () => (
+    <Button label="Primary" type="inverse-primary" />
+  ))
+  .add("Inverse Danger", () => <Button label="Danger" type="inverse-danger" />)
+  .add("Inverse Success", () => (
+    <Button label="Success" type="inverse-success" />
+  ))
+  .add("Inverse Warning", () => (
+    <Button label="Warning" type="inverse-warning" />
+  ))
+  .add("Inverse Default", () => (
+    <Button label="Default" type="inverse-default" />
+  ));
