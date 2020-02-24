cf84868ee73649c6c3fe2ee9108dbfc61e3e224e
vmipfdr <paynemw@yahoo.com>
2020-02-24 15:00:55 -0500

    cleaned up buttons

--- src/components/Forms/Voucher.js
+++ src/components/Forms/Voucher.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Voucher.css";
 
-const Voucher = props => {
-  let classList = "";
-  if (props.size) {
-    classList += ` ${props.size}-voucher`;
+class Voucher extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
   }
-  return (
-    <div className={"voucher-complete" + classList}>
-      <input className={"voucher-input" + classList} label={props.label} />
-      <div className={"redeem-label" + classList}>Redeem</div>
-    </div>
-  );
-};
+  render() {
+    return (
+      <form className={"voucher-complete"}>
+        <input className={"voucher-input"} label={"Voucher"} />
+        <button className={"redeem-label"}>Redeem</button>
+      </form>
+    );
+  }
+}
 
 export default Voucher;
+
+// const Voucher = props => {
+//   let classList = "";
+//   if (props.size) {
+//     classList += ` ${props.size}-voucher`;
+//   }
+//   return (
+//     <div className={"voucher-complete" + classList}>
+//       <input className={"voucher-input" + classList} label={props.label} />
+//       <div className={"redeem-label" + classList}>Redeem</div>
+//     </div>
+//   );
+// };
+
+// export default Voucher;
